fe28fbe7429812080f697cb03abb1418aeab19ff
Ian Frantz <ian@ianfrantz.com>
2019-12-11 21:16:26 -0800

    F14 - dbatools and mssql-tools added into powershell 1. Initialization Script (#13)
    * Minor: Removed unnecessary lines from Powershell script.
    
    * Minor: Added Adventureworks Data Warehouse and Wide World Imports manual task
    
    * Minor: Experimenting with sqlcmd from mssql-tools

--- Database Software/Microsoft/Powershell-VSCode/1.Initialization_Script.ps1
+++ Database Software/Microsoft/Powershell-VSCode/1.Initialization_Script.ps1
@@ -3,7 +3,7 @@ docker pull mcr.microsoft.com/mssql/server
 #Sql Server cmd Tools
 docker pull mcr.microsoft.com/mssql-tools
 
-
+#NOTE: Relative paths is an open issue. $wd is used to get around the problem. You will need to set this to repo path: \Database\ Software\Microsoft\Powershell-VSCode\
 $wd = Get-Location
 #Set-up the Container:
 docker run `
@@ -14,28 +14,45 @@ docker run `
 -v $wd/DatabaseBackups:/src `
 -d mcr.microsoft.com/mssql/server:latest
 
+
 #-----Restore Database Backups with dbatools-----
 #Install dbatools
 Install-Module -Name dbatools 
 
 # Set the credentials for the SA Account. Connect to local MS SQL Server 2019
-$cred = Get-Credential
-Get-DbaCredential -SqlInstance localhost:1433
+$cred = Get-Credential -UserName sa
 
+#Restore Adventureworks2017
 Restore-DbaDatabase -SqlInstance localhost:1433 -SqlCredential $cred -Path /src/adventureworks/AdventureWorks2017.bak
 
-#Get Logs
+#Restore Adventureworks Data Warehouse
+Restore-DbaDatabase -SqlInstance localhost:1433 -SqlCredential $cred -Path /src/adventureworks/AdventureWorksDW2017.bak
+
+#MANUAL TASK: Restore Wide World Importers after moving it into $wd.
+Restore-DbaDatabase -SqlInstance localhost:1433 -SqlCredential $cred -Path /src/wideworldimporters/WideWorldImporters-Full.bak
+
+#-----Troubleshooting-----
+#Get Logs from Dba Tools
 Get-DbatoolsLog
 
-#-----Attempted Backup using mssql-tools-----
-#Code below doesn't work but one day might. F
+
+#-----Using mssql-tools-----
+#See version of SQL Server with mssql-tools sqlcmd 
+sqlcmd -S localhost -U sa -P F00B4rB4z! 
+#TYPE: SELECT @@VERSION 
+#TYPE: GO 
+
+#Execute SQL Scripts
+sqlcmd `
+-S localhost `
+-i $wd/DatabaseBackups/HappyScoopers_Demo/'01 - Create HappyScoopers_Demo.sql'
 
 # docker exec MSSQL-Latest /opt/mssql-tools/bin/sqlcmd `
 # -S localhost `
 # -U "SA" `
 # -P "SA_PASSWORD=F00B4rB4z!" 
-
 # -Q "BACKUP DATABASE [Production-Snapshot] TO DISK = N'/var/opt/mssql/backup/Production-Snapshot.bak' WITH NOFORMAT, NOINIT, NAME = 'Production-Snapshot-full', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
+
 
 #-----Below code is just interesting stuff I want to keep-----
 
