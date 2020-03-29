7dcf8e57cff7271799fb31bd46d0065cf4818ab4
ianfrantz <ian@ianfrantz.com>
2020-03-29 11:34:34 -0700

    Major: Scripted pull of WideWorldImporters from Microsoft

--- Database Software/Microsoft/Powershell-VSCode/2.dbatools_Restore_Backup.ps1
+++ Database Software/Microsoft/Powershell-VSCode/2.dbatools_Restore_Backup.ps1
@@ -7,7 +7,6 @@ $User = "sa"
 $PWord = ConvertTo-SecureString -String "F00B4rB4z!" -AsPlainText -Force
 $Cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 
-
 #-----RESTORATION-----
 #Restore Adventureworks2017
 Restore-DbaDatabase -SqlInstance localhost:1433 -SqlCredential $cred -Path /src/adventureworks/AdventureWorks2017.bak
@@ -15,14 +14,18 @@ Restore-DbaDatabase -SqlInstance localhost:1433 -SqlCredential $cred -Path /src/
 #Restore Adventureworks Data Warehouse
 Restore-DbaDatabase -SqlInstance localhost:1433 -SqlCredential $cred -Path /src/adventureworks/AdventureWorksDW2017.bak
 
-#MANUAL TASK: Restore Wide World Importers after moving it into $wd. (File too big for github)
+#Download Wide World Importers V1.0 BAK file from Microsoft
+$wideworldimporters = "https://github.com/microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak"
+cd $PSScriptRoot
+$output = Get-Location
+Invoke-WebRequest -Uri $wideworldimporters -OutFile $output\DatabaseBackups\WideWorldImporters\WideWorldImporters-Full.bak
+#Restore Wide World Importers
 Restore-DbaDatabase -SqlInstance localhost:1433 -SqlCredential $cred -Path /src/WideWorldImporters/WideWorldImporters-Full.bak
 
 
 #-----BACKUP-----
 #Backup Adventureworks2017 (Default Path if not specified: /var/opt/mssql/data)
 Backup-DbaDatabase -SqlInstance localhost:1433 -SqlCredential $cred -Path /src -Database AdventureWorks2017, Sales.Customer 
-
 
 #-----Troubleshooting-----
 #Get Logs from Dba Tools
